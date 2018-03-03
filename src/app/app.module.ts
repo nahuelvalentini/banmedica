c97ce844dfaad7213442eb65ac0c28d4665fe76e
Nahuel Valentini <nahuel.valentini@fusiona.cl>
2018-03-03 14:34:31 -0300

    basic workspace done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,41 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { StepsComponent } from './components/steps/steps.component';
 
-
+const appRoutes: Routes = [
+  // { path: 'home', component: HomeComponent },
+  // { path: 'hero/:id',      component: HeroDetailComponent },
+  {
+    path: 'home',
+    component: HomeComponent,
+    data: { title: 'Inicio' }
+  },
+  {
+    path: 'pasos',
+    component: StepsComponent,
+    data: { title: 'Pasos' }
+  },
+  { path: 'p2',
+    redirectTo: '/home',
+    pathMatch: 'full'
+  },
+  { path: '**', component: HomeComponent }
+];
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    StepsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(
+      appRoutes,
+      // { enableTracing: true } // <-- debugging purposes only
+    )
   ],
   providers: [],
   bootstrap: [AppComponent]
